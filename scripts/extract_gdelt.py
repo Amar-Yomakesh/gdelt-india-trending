0da0e88a851eac7a34e576366f7816e0a62eceed
MasterShifu-NJC <84331385+AmarYoamakesh@users.noreply.github.com>
2025-03-11 18:58:23 +0530

    feat: extract articles

--- scripts/extract_gdelt.py
+++ scripts/extract_gdelt.py
@@ -2,14 +2,31 @@ from datetime import datetime, timedelta
 from gdeltdoc import GdeltDoc, Filters
 from os import path
 import yaml
+import requests
 
-configFile = path.abspath(path.join(__name__ ,"../../config/config.yaml"))
+# for running in jupiter notebook
+configFile = path.abspath(path.join(__name__, "../../config/config.yaml"))
+# configFile = "config/config.yaml"
 try:
     with open(f'{configFile}', 'r') as f:
         config = yaml.safe_load(f)
 except:
     print("config file not be loaded. exiting")
     exit(0)
+
+
+def themes_lookup():
+    theme_config = config['gdelt_api']["themes"]
+    if theme_config["lookup"] is True:
+        themesUrl = theme_config['url']
+        result = requests.get(themesUrl).content
+        result = result.decode(encoding="utf-8")
+        themes = []
+        for lines in result.split('\n'):
+            themes.append(lines.split('\t')[0])
+        return themes
+    else:
+        return theme_config["standardThemes"]
 
 
 def getFilters(theme):
@@ -22,14 +39,14 @@ def getFilters(theme):
     )
     return f
 
-gd = GdeltDoc()
-
 
 def extract_gdelt_timeline_bytheme(theme):
+    gd = GdeltDoc()
     filter = getFilters(theme)
     return gd.timeline_search("timelinevolraw", filter)
 
-def extract_gdelt_articles():
+
+def extract_gdelt_articles(theme):
+    gd = GdeltDoc()
     filter = getFilters(theme)
     return gd.article_search(filter)
-
