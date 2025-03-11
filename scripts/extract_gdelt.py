4760bdeb0ab603f14cba8063d6dff575a94c651d
MasterShifu-NJC <84331385+AmarYoamakesh@users.noreply.github.com>
2025-03-11 12:25:04 +0530

    feat: data transformation

--- scripts/extract_gdelt.py
+++ scripts/extract_gdelt.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
 from gdeltdoc import GdeltDoc, Filters
+from os import path
 import yaml
 
-configFile = 'config/config.yaml'
+configFile = path.abspath(path.join(__name__ ,"../../config/config.yaml"))
 try:
     with open(f'{configFile}', 'r') as f:
         config = yaml.safe_load(f)
@@ -21,8 +22,14 @@ def getFilters(theme):
     )
     return f
 
+gd = GdeltDoc()
 
-def extract_gdelt_bytheme(theme):
-    gd = GdeltDoc()
+
+def extract_gdelt_timeline_bytheme(theme):
     filter = getFilters(theme)
     return gd.timeline_search("timelinevolraw", filter)
+
+def extract_gdelt_articles():
+    filter = getFilters(theme)
+    return gd.article_search(filter)
+
--- scripts/main.py
+++ scripts/main.py
@@ -5,5 +5,6 @@ from datetime import datetime
 
 for theme in gdelt.config["themes"]:
     pd = gdelt.extract_gdelt_bytheme(theme)
-    key = "files/"+datetime.now().strftime("%Y-%m-%d")+"_"+theme+".csv"
-    aws.writeToS3(pd, key)
+    # key = "files/"+datetime.now().strftime("%Y-%m-%d")+"_"+theme+".csv"
+    # aws.writeToS3(pd, key)
+    print(pd)
